fc6934250e5d373a2fc200a198c583a3b7400e34
Hiroaki Fukuda <hiroaki@java2.jp>
2016-04-14 13:47:50 +0900

    add Stat.java

---
+++ src/ve3/os/Stat.java
@@ -0,0 +1,17 @@
+package ve3.os;
+
+public class Stat {
+	
+	public int dev;
+	public int inode;
+	public int permission;
+	public int link;
+	public int uid;
+	public int gid;
+	public int size;
+	public int rdev;
+	public int atime;
+	public int mtime;
+	public int ctime;
+
+}
